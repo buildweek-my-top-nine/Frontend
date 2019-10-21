d7b4063f54b229fbedf874dc048a5959eec7d4ef
Rayven <rayven.burns@gmail.com>
2019-10-21 15:27:30 -0400

    added login to app.js

--- top-nine/src/App.js
+++ top-nine/src/App.js
@@ -1,15 +1,18 @@
 import React from 'react';
 import './App.css';
-
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import Login from './components/Login';
-
-
 
 function App() {
   return (
+    <Router>
     <div className="App">
+
       <h1>Top Nine</h1>
+
+      <Route exact path ="/" component = {Login} />
     </div>
+    </Router>
   );
 }
 
