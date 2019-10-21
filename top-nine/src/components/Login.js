86802b60396180c1e014ea3c53967b5e5494a2f2
Rayven <rayven.burns@gmail.com>
2019-10-21 15:12:08 -0400

    done with login

---
+++ top-nine/src/components/Login.js
@@ -0,0 +1,68 @@
+import React, { useState } from 'react';
+import { axiosWithAuth } from '../utils/axiosWithAuth';
+
+
+
+const Login = (props) => {
+
+    const [credentials, setCredentials] = useState({
+
+        username: '',
+        password: '',
+    });
+
+    const handleChange = (e) => {
+        setCredentials({
+            ...credentials,
+            [e.target.name] : e.target.value
+        })
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        axiosWithAuth()
+        .post()
+        .then(response => {
+            console.log(response);
+            localStorage.setItem('token', response.data.payload);
+            props.history.push();
+        })
+        .catch(error => {
+            console.log(error);
+        })
+    }
+
+    return (
+
+        <>
+        <h1>Please Sign In!</h1>
+
+        <form onSubmit = {handleSubmit}>
+
+            <input
+                type = "text"
+                name = "username"
+                placeholder = "Username"
+                onChange = {handleChange}
+                value = {credentials.username}
+            />
+
+            <input
+                type = "password"
+                name = "password"
+                placeholder = "Password"
+                onChange = {handleChange}
+                value = {credentials.password}
+            />
+
+            <button>Submit</button>
+
+        </form>
+
+
+        </>
+    );
+};
+
+export default Login;
